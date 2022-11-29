9bf57dc25cea385178478cde8a0ce2ed682badc9
Bao Bao <nguyenbao.nix@gmail.com>
2022-11-29 15:32:03 +0700

    change app to index

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ const path = require("path");
 app.use("/public", express.static(path.join(__dirname, "./public")));
 
 const webRoute = require("./routes/web");
+app.get("/", (req, res) => {
+	return res.status(200).send({ message: "server gdent-app" });
+});
 app.use("/", webRoute);
 const port = process.env.serverport || 4000;
 app.listen(port, () => console.log(`Running on port ${port}...`));
